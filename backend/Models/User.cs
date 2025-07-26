55b3b2a86688ce1f516c34b2fbdd73c87d6f5b33
JoshNguyen <joshnguyen.work@gmail.com>
2025-07-26 15:07:25 +1200

    ADD: Database Annotation to User Domain Model

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -1,13 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace bulkbuy.api.Models
 {
-    public class User 
+    public class User
     {
+  
         public int Id { get; set; }
+
+        [Required]  
         public required string Username { get; set; }
 
+        [NotMapped]
+        [Required]
         public required string Password { get; set; }
         public string? PasswordHash { get; set; }
-       
+
+        // Empty constructor for EF Core
+        public User() { }
+
         public User(string username, string password)
         {
             Username = username;
