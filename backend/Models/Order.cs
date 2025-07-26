08bca53b04dbff36dc4f0e566a85ed21489b1830
JoshNguyen <joshnguyen.work@gmail.com>
2025-07-26 22:52:16 +1200

    Add: Annotation for Order domain model

--- backend/Models/Order.cs
+++ backend/Models/Order.cs
@@ -1,14 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace bulkbuy.api.Models
 {
     public class Order
     {
         public int Id { get; set; }
+
+        [Required]
         public required string Name { get; set; }
 
-        public required Group BuyingGroup { get; set; }
+        // Many-to-One relationship with Group (FK property)
+        [Required]
+        public int GroupId { get; set; }
 
+        [ForeignKey("GroupId")]
+        public required Group Group { get; set; }
+
+        // Many-to-One relationship with User (FK property)
+        [Required]
+        public int OwnerId { get; set; }
+
+        [ForeignKey("OwnerId")]
         public required User Owner { get; set; }
+
+        // Many-to-Many relationship with User (FK property)
         public List<User> Contributors { get; set; }
+
+        [Required]
         public int TargetAmount { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
